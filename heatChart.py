8203c0802d604ba838e765a0ba508db2fb16a539
wade <wadeanthony0100@gmail.com>
2015-05-13 00:32:13 -0400

    Implemented frequency color mapping

--- heatChart.py
+++ heatChart.py
@@ -23,7 +23,44 @@ dFill = PatternFill(patternType=fills.FILL_SOLID, fgColor='00C63C')
 eFill = PatternFill(patternType=fills.FILL_SOLID, fgColor='5DCC00')
 fFill = PatternFill(patternType=fills.FILL_SOLID, fgColor='D1A300')
 gFill = PatternFill(patternType=fills.FILL_SOLID, fgColor='D60400')
+hFill = PatternFill(patternType=fills.FILL_SOLID, fgColor='ffffff')
 backFill = PatternFill(patternType=fills.FILL_SOLID, fgColor=BLACK)
+
+"""
+Cells will be colored in based on frequence of their correspondance
+as follows:
+
+1-2:    A
+2-4:    B
+4-8:    C
+8-16:   D
+16-32:  E
+32-64:  F
+64-128: G
+128+:   H
+
+"""
+
+
+class Correlation():
+    def __init__(self, meaning, arrow_type):
+        self.meaning = meaning
+        self.arrow_type = int(arrow_type)
+        self.frequency = 1
+
+    def __repr__(self):
+        return "Corr(meaning="+str(self.meaning)+", arrow_type="+str(self.arrow_type)+", frequency="+str(self.frequency)+" )"
+
+    def __eq__(self, other):
+        return self.arrow_type == other.arrow_type
+
+    def addFreq(self):
+        """
+        add one to this instance of a correlation
+        :return: Nonetype
+        """
+        self.frequency += 1
+
 
 #collect I/O data
 input_name = input("Input file name- This must be placed \nin the directory above this script: ")
@@ -48,8 +85,8 @@ num_rows = data_sheet.max_row
 #initialize a dictionary of meaning strings
 #data will be stored as a mapping of meaning string keys
 #to lists of arrows for which the meaning exists
-meanings_to_lst_arrow = {}
-
+meanings_to_lst_arrows = {}
+meanings_to_lst_corr_objs = {}
 max_arrow = 0
 
 for x in range(2, num_rows+1):
@@ -59,13 +96,18 @@ for x in range(2, num_rows+1):
         meaning = meaning.strip()
     if arrow > max_arrow:
         max_arrow = arrow
-    if meaning in meanings_to_lst_arrow:
-        if not arrow in meanings_to_lst_arrow[meaning]:
-            meanings_to_lst_arrow[meaning].append(arrow)
+    if meaning in meanings_to_lst_arrows:
+        if not arrow in meanings_to_lst_arrows[meaning]:
+            meanings_to_lst_arrows[meaning].append(arrow)
+        for corr in meanings_to_lst_corr_objs[meaning]:
+            if corr == Correlation("", arrow):
+                corr.addFreq()
     else:
-        meanings_to_lst_arrow[meaning] = [arrow]
+        meanings_to_lst_arrows[meaning] = [arrow]
+        new_corr = Correlation(meaning, arrow)
+        meanings_to_lst_corr_objs[meaning] = [new_corr]
 
-#print(meanings_to_lst_arrow)
+print(meanings_to_lst_corr_objs)
 
 #make a new Worksheet
 new_book = Workbook()
@@ -76,20 +118,48 @@ for x in range(1, max_arrow+1):
     new_sheet[get_column_letter(x + 1) + "1"] = x
 
 #style all spaces black
-for x in range(2, len(meanings_to_lst_arrow) + 1):
+for x in range(2, len(meanings_to_lst_arrows) + 1):
     for y in range(2, max_arrow + 2):
         new_sheet[str(get_column_letter(y)) + str(x)].style = Style(fill=backFill)
 
 #get an alphabetized list to iterate through
-keys_list = list(meanings_to_lst_arrow.keys())
+keys_list = list(meanings_to_lst_arrows.keys())
 keys_list = [x for x in keys_list if x != None]
 keys_list.sort()
 index = 2
 for key in keys_list:
     new_sheet["A" + str(index)] = key
-    for num in meanings_to_lst_arrow[key]:
-        print("Plotting " + str(key) + " to " + str(get_column_letter(num + 1)) + str(index))
-        new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=aFill)
+    for num in meanings_to_lst_arrows[key]:
+        #print("Plotting " + str(key) + " to " + str(get_column_letter(num + 1)) + str(index))
+        tier_num = 0
+        for corr in meanings_to_lst_corr_objs[key]:
+            if corr == Correlation("", num):
+                tier_num = corr.frequency
+        print("Tire num = " + str(tier_num))
+        if tier_num <= 2:
+            new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=aFill)
+
+        elif tier_num <= 4:
+            new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=bFill)
+
+        elif tier_num <= 8:
+            new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=cFill)
+
+        elif tier_num <= 16:
+            new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=dFill)
+
+        elif tier_num <= 32:
+            new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=eFill)
+
+        elif tier_num <= 64:
+            new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=fFill)
+
+        elif tier_num <= 128:
+            new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=gFill)
+
+        else: #128+
+            new_sheet[str(get_column_letter(num + 1)) + str(index)].style = Style(fill=hFill)
+
     index += 1
 
 #save the new workbook with the given output filename
